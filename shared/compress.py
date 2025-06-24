6a4b19d68f206e518c30cc830a1642774af27a5e
TaoZeyu <i@taozeyu.com>
2025-06-24 17:59:09 +0800

    feat(tasks): add pack block logic

---
+++ shared/compress.py
@@ -0,0 +1,26 @@
+from typing import Any, Generator, Callable
+from pathlib import Path
+from zipfile import ZipFile, ZIP_DEFLATED
+
+
+def compress_with_zip(
+    title: str,
+    raw_files: list[Path],
+    output_path: Path,
+    progress: Callable[[int], None],
+  ) -> None:
+
+  with ZipFile(output_path, "w", ZIP_DEFLATED) as zip:
+    for i, (file_name, raw_path) in enumerate(_iter_files(title, raw_files)):
+      zip.write(raw_path, file_name)
+      progress(i + 1)
+
+def _iter_files(title: str, raw_files: list[Path]) -> Generator[tuple[str, Path], Any, None]:
+  max_digits = len(str(len(raw_files)))
+  for index, raw_path in enumerate(raw_files):
+    file_name = str(index).zfill(max_digits)
+    if title:
+      file_name: str = f"{title}-{file_name}"
+    suffix = "".join(raw_path.suffixes)
+    file_name = f"{file_name}{suffix}"
+    yield file_name, raw_path
---
+++ tasks/pack/__init__.py
@@ -0,0 +1,66 @@
+from curses import raw
+from pathlib import Path
+from oocana import Context
+from typing import cast, Literal
+
+from shared.compress import compress_with_zip
+
+
+#region generated meta
+import typing
+class Inputs(typing.TypedDict):
+  images: list[str]
+  title: str | None
+  format: typing.Literal["cbz", "cbr", "epub", "pdf"] | None
+  pack_path: str | None
+class Outputs(typing.TypedDict):
+  pack_path: str
+#endregion
+
+_Suffix = Literal[".cbz", ".cbr", ".epub", ".pdf"]
+_SUFFIX_TUPPLE: tuple[_Suffix, ...] = (".cbz", ".cbr", ".epub", ".pdf")
+
+def main(params: Inputs, context: Context) -> Outputs:
+  format = params["format"]
+  pack_path = params["pack_path"]
+  suffix: _Suffix = ".cbz"
+
+  if format is not None:
+    suffix = cast(_Suffix, f".{format}")
+
+  if pack_path is None:
+    workspace_path = Path(context.session_dir) / "manga-tools"
+    pack_path = workspace_path / f"{context.job_id}{suffix}"
+    pack_path.parent.mkdir(parents=True, exist_ok=True)
+
+  else:
+    pack_path = Path(pack_path)
+    pack_suffix = pack_path.suffix.lower()
+    if pack_suffix in _SUFFIX_TUPPLE:
+      suffix = cast(_Suffix, pack_suffix)
+    else:
+      pack_path = pack_path.with_suffix(suffix)
+
+  raw_paths: list[Path] = []
+  for path in params["images"]:
+    path = Path(path)
+    if not path.exists():
+      raise ValueError(f"Image path {path} does not exist")
+    if not path.is_file():
+      raise ValueError(f"Image path {path} is not a file")
+    raw_paths.append(path)
+
+  title = (params["title"] or "").strip()
+  if suffix == ".cbz":
+    compress_with_zip(
+      title=title,
+      raw_files=raw_paths,
+      output_path=pack_path,
+      progress=lambda p: context.report_progress(
+        progress=100*float(p)/len(raw_paths),
+      ),
+    )
+  else:
+    raise ValueError(f"invalid suffix {suffix}")
+
+  return { "pack_path": str(pack_path) }
