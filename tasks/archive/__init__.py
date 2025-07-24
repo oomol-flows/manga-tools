c396eaca4f581a0b40eb104421bb858f9e2e64bb
TaoZeyu <i@taozeyu.com>
2025-07-24 15:25:45 +0800

    feat(tasks): rename to archive_path

--- tasks/archive/__init__.py
+++ tasks/archive/__init__.py
@@ -14,12 +14,12 @@ import typing
 class Inputs(typing.TypedDict):
   images: list[str]
   format: typing.Literal["cbz", "epub", "pdf"] | None
-  pack_path: str | None
+  archive_path: str | None
   title: str | None
   author: str | None
   reading_order: typing.Literal["to-right", "to-left"]
 class Outputs(typing.TypedDict):
-  pack_path: str
+  archive_path: str
 #endregion
 
 
@@ -27,13 +27,13 @@ _Suffix = Literal[".cbz", ".epub", ".pdf"]
 _SUFFIX_TUPPLE: tuple[_Suffix, ...] = (".cbz", ".epub", ".pdf")
 
 def main(params: Inputs, context: Context) -> Outputs:
-  pack_path = _derive_pack_path(params, context)
-  suffix: _Suffix = cast(_Suffix, pack_path.suffix)
+  archive_path = _derive_archive_path(params, context)
+  suffix: _Suffix = cast(_Suffix, archive_path.suffix)
   title = _normalize_str(params["title"])
   author = _normalize_str(params["author"])
 
-  if title is None and bool(params["pack_path"]):
-    title = sanitize_filename(pack_path.stem)
+  if title is None and bool(params["archive_path"]):
+    title = sanitize_filename(archive_path.stem)
 
   raw_paths: list[Path] = []
   for path in params["images"]:
@@ -44,13 +44,13 @@ def main(params: Inputs, context: Context) -> Outputs:
       raise ValueError(f"Image path {path} is not a file")
     raw_paths.append(path)
 
-  pack_path= _clean_path(pack_path)
+  archive_path= _clean_path(archive_path)
 
   if suffix == ".cbz":
     archive_with_zip(
       title=title,
       raw_files=raw_paths,
-      output_path=pack_path,
+      output_path=archive_path,
       progress=lambda p: context.report_progress(
         progress=100*float(p)/len(raw_paths),
       ),
@@ -60,7 +60,7 @@ def main(params: Inputs, context: Context) -> Outputs:
       title=title,
       dpi=96,
       raw_files=raw_paths,
-      output_path=pack_path,
+      output_path=archive_path,
       progress=lambda p: context.report_progress(
         progress=100*float(p)/len(raw_paths),
       ),
@@ -73,34 +73,34 @@ def main(params: Inputs, context: Context) -> Outputs:
       title=title,
       author=author,
       image_paths=raw_paths,
-      output_path=pack_path,
+      output_path=archive_path,
       temp_path=temp_path,
       read_to_left=(params["reading_order"] == "to-left"),
       progress=lambda p: context.report_progress(100.0*p),
     )
-  return { "pack_path": str(pack_path) }
+  return { "archive_path": str(archive_path) }
 
-def _derive_pack_path(params: Inputs, context: Context) -> Path:
+def _derive_archive_path(params: Inputs, context: Context) -> Path:
   format = params["format"]
-  pack_path = params["pack_path"]
+  archive_path = params["archive_path"]
   suffix: _Suffix = ".cbz"
 
   if format is not None:
     suffix = cast(_Suffix, f".{format}")
 
-  if pack_path is None:
+  if archive_path is None:
     workspace_path = Path(context.session_dir) / "manga-tools"
-    pack_path = workspace_path / f"{context.job_id}{suffix}"
-    pack_path.parent.mkdir(parents=True, exist_ok=True)
+    archive_path = workspace_path / f"{context.job_id}{suffix}"
+    archive_path.parent.mkdir(parents=True, exist_ok=True)
   else:
-    pack_path = Path(pack_path)
-    pack_path_suffix = pack_path.suffix.lower()
+    archive_path = Path(archive_path)
+    pack_path_suffix = archive_path.suffix.lower()
     if pack_path_suffix not in _SUFFIX_TUPPLE:
-      pack_path = pack_path.with_suffix(pack_path_suffix + suffix)
+      archive_path = archive_path.with_suffix(pack_path_suffix + suffix)
     elif suffix != pack_path_suffix and format is not None:
       print("warnning: format is ignored when package_path's extension name is specified")
 
-  return pack_path
+  return archive_path
 
 def _normalize_str(param: str | None) -> str | None:
   if param is None:
